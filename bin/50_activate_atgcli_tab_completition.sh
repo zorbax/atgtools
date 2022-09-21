477a57f373f70ac460e69c7a8150f5fa04bd0a0b
Otoniel Maya <otto94@gmail.com>
2022-09-21 14:20:37 +0200

    rm old files

--- bin/50_activate_atgcli_tab_completition.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -n "${ZSH_VERSION-}" ]; then
-	autoload -Uz compinit && compinit && autoload bashcompinit && bashcompinit && source tab-qiime
-elif [ -n "${BASH_VERSION-}" ]; then
-	source tab-atg
-fi
-
